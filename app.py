ba9b63551f77c00cf5e8f0fa78f57e99c15deeb7
Tochukwu <tochukwusage4@gmail.com>
2021-02-06 13:46:43 +0100

    changed-port

--- app.py
+++ app.py
@@ -368,11 +368,11 @@ def approve(id):
     
 
 if __name__ == '__main__':
-    #import os
-    #HOST = os.environ.get('SERVER_HOST', 'localhost')
-    #try:
-    #    PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    #except ValueError:
-    #    PORT = 5555
-    #app.run(HOST, PORT)
-     app.run(debug=True, host='0.0.0.0')
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
+     #app.run(debug=True, host='0.0.0.0')
